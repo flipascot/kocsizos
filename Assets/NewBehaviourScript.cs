cfa99c91a99ed44475207d0a17dd39281433bc0e
Banfalvi_Tamas_Mark <tbanfalvi2000@gmail.com>
2023-12-01 19:49:16 +0100

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+
+
+
+
+public class NewBehaviourScript : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material WinMaterial;
+    [SerializeField] private Material LoseMaterial;
+    [SerializeField] private MeshRenderer FloorMeshRender;
+
+
+    public override void OnEpisodeBegin() {
+        transform.localPosition =Vector3.zero;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+       float moveX = actions.ContinuousActions[0];
+       float moveZ = actions.ContinuousActions[1];
+    
+    float moveSpeed = 1f;
+    transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+   // public override void Heuristic(in ActionBuffers actionsOut)
+   // {
+  //      ActionSegment<float> continuosActions= actionsOut.ContinuousActions;
+   //     continuosActions[0]=Input.GetAxisRaw("Horizontal");
+   //     continuosActions[1]=Input.GetAxisRaw("Vertical");
+   // }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.TryGetComponent<Goal>(out Goal goal)){
+        SetReward(+1f);
+        FloorMeshRender.material = WinMaterial;
+        EndEpisode();
+        }
+         if(other.TryGetComponent<Wall>(out Wall wall)){
+        SetReward(-1f);
+        FloorMeshRender.material = LoseMaterial;
+        EndEpisode();
+        }
+    }
+}
